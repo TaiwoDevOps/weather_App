8d16432da1c448e84d7b1ee65161cf75625fe250
Adisa Taiwo <adisataiwojoshua@gmail.com>
2019-04-26 16:25:06 +0100

    Building a weather app with flutter, just learning

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// 47ff98d24f0c272140048e8b6ed64d8f {my weather api key}
+// a364acef17be4b2db70215749181408 "APIXU.com weather api key"
+import 'package:flutter/material.dart';
+import './ui/klimatic.dart';
+
+
+void main(){
+  runApp(new MaterialApp(
+    title: 'Klimatic',
+    home: new Klimatic(),
+  ));
+}
---
+++ lib/util/util.dart
@@ -0,0 +1,2 @@
+final appId = "a364acef17be4b2db70215749181408";
+final defaultCity = "Spokane";
